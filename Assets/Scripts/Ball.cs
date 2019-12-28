eb155c683ae33b81a65f35f271def34ebd2dd877
Joy Karmoker <joykarmoker353@gmail.com>
2019-12-28 15:18:51 +0600

    Block Destroyed on collision with the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Config Parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 14.5f;
+
+    //State
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     void Start()
     {
@@ -15,6 +21,26 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if(hasStarted == false)
+        {
+            LockBallWithPaddle();
+            LaunchBallOnMouseClick();
+        }
+        
+       
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallWithPaddle()
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
